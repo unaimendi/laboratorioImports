50e76e3b0c6d79b05541999194fb7405ee27f4b5
Unai <umendizabal@gmail.com>
2024-03-25 13:00:39 +0100

    version de JP

---
+++ src/barajaInicial.ts
@@ -0,0 +1,43 @@
+const barajaInicial = [
+	{
+		carta: "As",
+		valor: 1,
+	},
+	{
+		carta: "Dos",
+		valor: 2,
+	},
+	{
+		carta: "Tres",
+		valor: 3,
+	},
+	{
+		carta: "Cuatro",
+		valor: 4,
+	},
+	{
+		carta: "Cinco",
+		valor: 5,
+	},
+	{
+		carta: "Seis",
+		valor: 6,
+	},
+	{
+		carta: "Siete",
+		valor: 7,
+	},
+	{
+		carta: "Sota",
+		valor: 0.5,
+	},
+	{
+		carta: "Caballo",
+		valor: 0.5,
+	},
+	{
+		carta: "Rey",
+		valor: 0.5,
+	},
+];
+export default barajaInicial;
---
+++ src/motor.ts
@@ -0,0 +1,60 @@
+import { barajaInicial, partida } from "./modelo";
+
+const posicionCartaRandom = (): number => Math.floor(Math.random() * partida.barajaRestante.length);
+
+export const sacarCarta = (): void => {
+	const nuevaCarta = partida.barajaRestante.splice(posicionCartaRandom(), 1)[0];
+
+	partida.carta = nuevaCarta;
+};
+
+export const calculaPuntuacion = (): void => {
+	if (partida.carta.valor) {
+		partida.puntuacion = partida.puntuacion + partida.carta.valor;
+	}
+};
+
+export const generarMensajePlantarse = (): void => {
+	switch (true) {
+		case partida.puntuacion < 0.5:
+			partida.mensaje = "Al menos juega una carta cagón";
+			break;
+		case partida.puntuacion < 4:
+			partida.mensaje = "Has sido muy conservador";
+			break;
+		case partida.puntuacion < 6:
+			partida.mensaje = "Te ha entrado el canguelo eh?";
+			break;
+		case partida.puntuacion <= 7:
+			partida.mensaje = "Casi casi...";
+			break;
+		default:
+			partida.mensaje = "";
+			break;
+	}
+};
+
+export const comprobarEstadoPartida = (): void => {
+	switch (true) {
+		case partida.puntuacion === 7.5:
+			partida.estado = "gana";
+			break;
+		case partida.puntuacion > 7.5:
+			partida.estado = "pierde";
+			break;
+		default:
+			partida.estado = "enProgreso";
+			break;
+	}
+};
+
+export const resetPartida = () => {
+	partida.puntuacion = 0;
+	partida.carta = {
+		valor: null,
+		imagenUrl: "/src/assets/back.jpg",
+	};
+	partida.mensaje = "";
+	partida.estado = "noIniciada";
+	partida.barajaRestante = barajaInicial;
+};
